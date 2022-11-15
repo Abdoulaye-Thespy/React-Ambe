f4b3fea1f2aeb5b211729e0280c9d56cb1605665
Abdoulaye-MAcOS <njigouhrazak@iut-dhaka.edu>
2022-11-15 10:47:19 +0100

    chore: Add Boostrap to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import  Nav from './containers/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Datagend
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coming soon
-        </a>
-      </header>
-    </div>
+
+    <>
+    <Nav />
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Datagend
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Coming soon
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
